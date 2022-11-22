f3c34b9a87fb7007a26381ccf5984875d40bb9b4
Zoe <elias.m.decorvet@gmail.com>
2022-11-22 10:33:51 +0100

    Added mald code, so u can see it isn't a trojan ;)

---
+++ Form1.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Forms;
+
+namespace MentalSupport
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            System.IO.Stream str = Properties.Resources.DANCE_BITCH;
+            System.Media.SoundPlayer snd = new System.Media.SoundPlayer(str);
+            snd.PlayLooping();
+        }
+    }
+}
